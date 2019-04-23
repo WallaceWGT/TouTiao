4759857f9e88008e793f44b60cf25f7bdffc97a9
wallace.wang <freestylewallace@163.com>
2019-04-23 11:34:45 +0800

    first commit

---
+++ spider.py
@@ -0,0 +1,149 @@
+import json
+import os
+from urllib.parse import urlencode
+import pymongo
+import requests
+from bs4 import BeautifulSoup
+from requests.exceptions import ConnectionError
+import re
+from multiprocessing import Pool
+from hashlib import md5
+from json.decoder import JSONDecodeError
+from config import *
+
+client = pymongo.MongoClient(MONGO_URL, connect=False)
+db = client[MONGO_DB]
+
+
+def get_page_index(offset, keyword):
+    """
+    更加index的url获取ajax请求返回的组图信息
+    :param offset:
+    :param keyword:
+    :return:
+    """
+    data = {
+        'autoload': 'true',
+        'count': 20,
+        'cur_tab': 3,
+        'format': 'json',
+        'keyword': keyword,
+        'offset': offset,
+    }
+    params = urlencode(data)
+    base = 'http://www.toutiao.com/search_content/'
+    url = base + '?' + params
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.text
+        return None
+    except ConnectionError:
+        print('Error occurred')
+        return None
+
+
+def download_image(url):
+    """
+    获取图片内容
+    :param url:
+    :return:
+    """
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            save_image(response.content)
+        return None
+    except ConnectionError:
+        return None
+
+
+def save_image(content):
+    """
+    保存图片
+    :param content:
+    :return:
+    """
+    file_path = '{0}/{1}.{2}'.format(os.getcwd(), md5(content).hexdigest(), 'jpg')
+    if not os.path.exists(file_path):
+        with open(file_path, 'wb') as f:
+            f.write(content)
+
+
+def parse_page_index(text):
+    """
+    更加详情页获取单个图片的uri
+    :param text:
+    :return:
+    """
+    try:
+        data = json.loads(text)
+        if data and 'data' in data.keys():
+            for item in data.get('data'):
+                yield item.get('article_url')
+    except JSONDecodeError:
+        pass
+
+
+def get_page_detail(url):
+    """
+    获取详情页
+    :param url:
+    :return:
+    """
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.text
+        return None
+    except ConnectionError:
+        print('Error occurred')
+        return None
+
+
+def parse_page_detail(html, url):
+    soup = BeautifulSoup(html, 'lxml')
+    result = soup.select('title')
+    title = result[0].get_text() if result else ''
+    images_pattern = re.compile('gallery: JSON.parse\("(.*)"\)', re.S)
+    result = re.search(images_pattern, html)
+    if result:
+        data = json.loads(result.group(1).replace('\\', ''))
+        if data and 'sub_images' in data.keys():
+            sub_images = data.get('sub_images')
+            images = [item.get('url') for item in sub_images]
+            for image in images: download_image(image)
+            return {
+                'title': title,
+                'url': url,
+                'images': images
+            }
+
+
+def save_to_mongo(result):
+    """
+    保存到mongodb
+    :param result:
+    :return:
+    """
+    if db[MONGO_TABLE].insert(result):
+        print('Successfully Saved to Mongo', result)
+        return True
+    return False
+
+
+def main(offset):
+    text = get_page_index(offset, KEYWORD)
+    urls = parse_page_index(text)
+    for url in urls:
+        html = get_page_detail(url)
+        result = parse_page_detail(html, url)
+        if result: save_to_mongo(result)
+
+
+if __name__ == '__main__':
+    pool = Pool()
+    groups = ([x * 20 for x in range(GROUP_START, GROUP_END + 1)])
+    pool.map(main, groups)
+    pool.close()
+    pool.join()
